90970a89e8643e43e019240a0d1bc30b4b40c752
RAM-VATS <75200197+RAM-VATS@users.noreply.github.com>
2021-03-27 11:07:41 +0530

    Add files via upload

---
+++ Roof.js
@@ -0,0 +1,21 @@
+class roof{
+    constuctor(x,y,w,h){
+        this.x=x;
+        this.y=y;
+        this.w=w;
+        this.h=h;
+        this.body=Bodies.rectangle(x,y,w,h,options)
+       // this.width=width;
+       // this.height=height;
+        World.add(world,this.body)
+    }
+    display(){
+        var groundPos=this.body.position;
+ push();
+ translate(groundPos.x,groundPos.y)
+ rectMode(CENTER);
+ fill(128,128,128)
+rect(0,0,this.w,this.h);
+pop();
+}
+}
